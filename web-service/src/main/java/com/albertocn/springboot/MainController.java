9db1edc657f491ec93cd00815e352a3ac4baec9f
Gromenawuer <gromenawuer12@gmail.com>
2021-01-22 20:40:59 +0100

    Fixing reviewers comments

--- web-service/src/main/java/com/albertocn/springboot/MainController.java
+++ web-service/src/main/java/com/albertocn/springboot/MainController.java
@@ -5,26 +5,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.YearMonth;
+import java.net.URI;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping(path="/tasks")
 public class MainController {
     @Autowired
     private TasksRepository tasksRepository;
-
-    @PostMapping(path="/add")
     public ResponseEntity<String> addNewTask (@RequestParam Users user_id, @RequestParam LocalDateTime start, @RequestParam LocalTime duration, @RequestParam Task_Types task_type_id, @RequestParam String description, @RequestParam Weather_Tasks weather_task_id) {
 
         Tasks t = new Tasks();
@@ -34,35 +25,33 @@ public class MainController {
         t.setTask_type_id(task_type_id);
         t.setDescription(description);
         t.setWeather_task_id(weather_task_id);
-        tasksRepository.save(t);
-        return new ResponseEntity<>("Saved!", HttpStatus.OK);
+        t = tasksRepository.save(t);
+        URI uri = URI.create(getTaskById(t.getId()).toString());
+        return ResponseEntity.created(uri).build();
     }
-
-    @GetMapping(path="/all")
+    @GetMapping("/tasks")
     public ResponseEntity<Iterable<Tasks>> getAllTasks() {
         return new ResponseEntity<>(tasksRepository.findAll(),HttpStatus.OK);
     }
-
-    @GetMapping(path="/monthlyTasks")
-    public ResponseEntity<Iterable<Tasks>>getMonthlyTasks(@RequestParam(value = "date") YearMonth date) {
-        Iterable<Tasks> tr = getAllTasks().getBody();
-        List<Tasks> tasks = new ArrayList<>();
-        tr.forEach((task) -> {
-            if (task.getStart().getMonth() == date.getMonth() &&
-                    task.getStart().getYear() == date.getYear()) {
-                tasks.add(task);
-            }
-        });
-        return new ResponseEntity<>(tasks,HttpStatus.OK);
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Object> getTaskById (@PathVariable Integer id) {
+        return new ResponseEntity<>(tasksRepository.findById(id),HttpStatus.OK);
     }
-
-    @GetMapping(path="/dailyTasks")
-    public ResponseEntity<Iterable<Tasks>> getDailyTasks(@RequestParam(value = "localDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate localDate) {
+    @GetMapping("/tasks/")
+    public ResponseEntity<Iterable<Tasks>> getFilterTasks(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+                                                         @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+                                                         @RequestParam(required = false) String type) {
         Iterable<Tasks> tr = getAllTasks().getBody();
         List<Tasks> tasks = new ArrayList<>();
         tr.forEach((task) -> {
-            if (task.getStart().toLocalDate().equals(localDate)) {
-                tasks.add(task);
+            if ((task.getStart().toLocalDate().equals(from)||task.getStart().toLocalDate().isAfter(from)) &&
+                    (task.getStart().toLocalDate().equals(to)||task.getStart().toLocalDate().isBefore(to))) {
+                if(type!=null){
+                    if (task.getTask_type_id().getName().equals(type)) tasks.add(task);
+                }
+                else {
+                    tasks.add(task);
+                }
             }
         });
         return new ResponseEntity<>(tasks,HttpStatus.OK);
