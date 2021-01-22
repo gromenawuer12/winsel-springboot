cf81775d933b90117db74159aec7c27cbb84b916
Gromenawuer <gromenawuer12@gmail.com>
2021-01-22 13:35:42 +0100

    Fixing reviewers comments

--- web-service/src/main/java/com/albertocn/springboot/MainController.java
+++ web-service/src/main/java/com/albertocn/springboot/MainController.java
@@ -2,6 +2,8 @@ package com.albertocn.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +25,7 @@ public class MainController {
     private TasksRepository tasksRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addNewTask (@RequestParam Users user_id, @RequestParam LocalDateTime start,@RequestParam LocalTime duration,@RequestParam Task_Types task_type_id,@RequestParam String description,@RequestParam Weather_Tasks weather_task_id) {
+    public ResponseEntity<String> addNewTask (@RequestParam Users user_id, @RequestParam LocalDateTime start, @RequestParam LocalTime duration, @RequestParam Task_Types task_type_id, @RequestParam String description, @RequestParam Weather_Tasks weather_task_id) {
 
         Tasks t = new Tasks();
         t.setUser_id(user_id);
@@ -33,17 +35,17 @@ public class MainController {
         t.setDescription(description);
         t.setWeather_task_id(weather_task_id);
         tasksRepository.save(t);
-        return "Saved";
+        return new ResponseEntity<>("Saved!", HttpStatus.OK);
     }
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<Tasks> getAllTasks() {
-        return tasksRepository.findAll();
+    public ResponseEntity<Iterable<Tasks>> getAllTasks() {
+        return new ResponseEntity<>(tasksRepository.findAll(),HttpStatus.OK);
     }
 
     @GetMapping(path="/monthlyTasks")
-    public @ResponseBody Iterable<Tasks> getMonthlyTasks(@RequestParam(value = "date") YearMonth date) {
-        Iterable<Tasks> tr = getAllTasks();
+    public ResponseEntity<Iterable<Tasks>>getMonthlyTasks(@RequestParam(value = "date") YearMonth date) {
+        Iterable<Tasks> tr = getAllTasks().getBody();
         List<Tasks> tasks = new ArrayList<>();
         tr.forEach((task) -> {
             if (task.getStart().getMonth() == date.getMonth() &&
@@ -51,18 +53,18 @@ public class MainController {
                 tasks.add(task);
             }
         });
-        return tasks;
+        return new ResponseEntity<>(tasks,HttpStatus.OK);
     }
 
     @GetMapping(path="/dailyTasks")
-    public @ResponseBody Iterable<Tasks> getDailyTasks(@RequestParam(value = "localDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate localDate) {
-        Iterable<Tasks> tr = getAllTasks();
+    public ResponseEntity<Iterable<Tasks>> getDailyTasks(@RequestParam(value = "localDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate localDate) {
+        Iterable<Tasks> tr = getAllTasks().getBody();
         List<Tasks> tasks = new ArrayList<>();
         tr.forEach((task) -> {
             if (task.getStart().toLocalDate().equals(localDate)) {
                 tasks.add(task);
             }
         });
-        return tasks;
+        return new ResponseEntity<>(tasks,HttpStatus.OK);
     }
 }
