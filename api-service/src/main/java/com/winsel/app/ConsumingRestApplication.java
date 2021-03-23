08d7395efee63916766ed764eb40e46a79a059cd
Gromenawuer <gromenawuer12@gmail.com>
2021-03-23 21:49:02 +0100

    Featuring service discovery(Eureka)

--- api-service/src/main/java/com/winsel/app/ConsumingRestApplication.java
+++ api-service/src/main/java/com/winsel/app/ConsumingRestApplication.java
@@ -1,14 +1,14 @@
 package com.winsel.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.winsel.controller")
 @ComponentScan(basePackages = "com.winsel.dto")
+@EnableEurekaClient
 public class ConsumingRestApplication {
 
     public static void main(String[] args) {
--- web-service/src/main/java/com/winsel/app/SpringApplication.java
+++ web-service/src/main/java/com/winsel/app/SpringApplication.java
@@ -2,10 +2,12 @@ package com.winsel.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = "com.winsel.controller")
 @ComponentScan(basePackages = "com.winsel.service")
 @ComponentScan(basePackages = "com.winsel.dto")
