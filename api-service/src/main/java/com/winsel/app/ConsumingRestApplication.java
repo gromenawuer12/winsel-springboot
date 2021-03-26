cd03781d519d89eb9767db14f26ffeeebd9ba880
Gromenawuer <gromenawuer12@gmail.com>
2021-03-26 14:56:11 +0100

    Fixing eureka implementation

--- api-service/src/main/java/com/winsel/app/ConsumingRestApplication.java
+++ api-service/src/main/java/com/winsel/app/ConsumingRestApplication.java
@@ -2,7 +2,6 @@ package com.winsel.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
