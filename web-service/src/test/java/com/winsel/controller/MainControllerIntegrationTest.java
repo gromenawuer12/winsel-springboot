57963a35c12026b93c9e794a24193cf49af82671
Gromenawuer <gromenawuer12@gmail.com>
2021-05-31 19:24:36 +0200

    Feature integration tests

---
+++ web-service/src/test/java/com/winsel/controller/MainControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.winsel.controller;
+
+import com.winsel.app.SpringApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = { "classpath:application.yml" })
+@WebAppConfiguration
+public class MainControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void givenGreetURI_whenMockMVC_thenVerifyResponse() throws Exception{
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/tasks"))
+                .andDo(print()).andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        assertEquals("application/json;charset=UTF-8",
+                mvcResult.getResponse().getContentType());
+    }
+}
